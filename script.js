1ec04798f000bd369396ddf3126c3a7190631367
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2024-02-15 21:36:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
   // your code here
+	if(s.length===0){
+		return '';
+	}
+	else{
+		for(let i = 0; i<s.length; i++){
+			if(s[i]===' '){
+				for( let p = 0; p<=i; p++){
+					return s[p];
+				}
+			}
+		}
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
